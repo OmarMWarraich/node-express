710a8901d599d497e959a5ff32dbc90ec2c5c44d
OmarMWarraich <earthling986@gmail.com>
2023-06-17 19:03:30 +0500

    feat: Methods-Put and Delete

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -24,6 +24,40 @@ app.post('/api/people', (req, res) => {
     
 });
 
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+  
+    const person = people.find((person) => person.id === Number(id))
+  
+    if (!person) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `no person with id ${id}` })
+    }
+    const newPeople = people.map((person) => {
+      if (person.id === Number(id)) {
+        person.name = name
+      }
+      return person
+    })
+    res.status(200).json({ success: true, data: newPeople })
+  })
+  
+  app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if (!person) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `no person with id ${req.params.id}` })
+    }
+    const newPeople = people.filter(
+      (person) => person.id !== Number(req.params.id)
+    )
+    return res.status(200).json({ success: true, data: newPeople })
+  })
+  
+
 
 app.post('/login', (req, res) => {
     const { name } = req.body;
