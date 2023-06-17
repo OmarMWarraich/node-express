0af764d6823057a15259cea52627c43a94a8a264
OmarMWarraich <earthling986@gmail.com>
2023-06-17 15:15:20 +0500

    feat: All Static-SSR

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Home Page');
-});
+// setup static and middleware
+app.use(express.static('./navbar-app/public'));
+
+/* app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+    // Adding to static folder
+    // SSR
+}); */
 
 app.get('/about', (req, res) => {
     res.status(200).send('About Page');
