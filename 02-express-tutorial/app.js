3539704c4e9b6e092b6f8f7450fca1739f66e403
OmarMWarraich <earthling986@gmail.com>
2023-06-17 16:59:41 +0500

    feat: Params Extra Info

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,12 +6,18 @@ app.get('/', (req, res) => {
     res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
 });
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product;
-        return { id, name, image };
-    });
-    res.json(newProducts);
+app.get('/api/products/:productId', (req, res) => {
+    const { productId } = req.params;
+    const singleProduct = products.find((product) => product.id === Number(productId));
+    if (!singleProduct) {
+        return res.status(404).send('Product Does Not Exist');
+    }
+    return res.json(singleProduct);
+});
+
+app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
+    console.log(req.params);
+    res.send('hello world');
 });
 
 app.listen(5000, () => {
